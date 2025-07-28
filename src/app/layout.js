89f94c69e6cd3966adb6a6a9b5c0681ad302a63a
Samarth Bhatia <samarthbhatia77@gmail.com>
2025-07-28 22:32:52 +0530

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import "./globals.css";
 export const metadata = {
   title: "Gust",
+  icons: {
+    icon: "/favicon.png",
+  },
   description: "Gust is a sleek and responsive weather application built using React, Next.js, and Tailwind CSS. It provides live weather updates such as temperature, wind speed, and status icons for cities worldwide  all presented in a clean, modern UI with smooth interactivity.",
 };
 
